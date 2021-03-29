6796d429fe7617ab7db21980ab76ab4a5974e443
W3ndig0u0 <jingxiang.xu@elev.ga.ntig.se>
2021-03-29 12:58:58 +0200

    YEP

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+
+  public NavMeshAgent agent;
+
+  public Transform player;
+
+  public LayerMask whatIsGround, whatIsPlayer;
+
+  public float health = 500f;
+
+  // !Patrolling
+  public Vector3 walkPoint;
+  bool walkpointSet;
+  public float walkPointRange;
+
+  // !States
+  public float sightRange, attackRange;
+  public bool playerInSightRange, playerInAttackRange;
+
+  //  !Attacking
+  public float timeBetweenAttack;
+  bool alreadyAttacked;
+
+  void Awake()
+  {
+    player = GameObject.Find("PlayerContainer").transform;
+    agent = GetComponent<NavMeshAgent>();
+  }
+
+  void Update()
+  {
+    //   !Kolla efter syn och attackRange
+    playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+    playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+
+    // !AI Status
+    if (playerInSightRange && playerInAttackRange)
+    {
+      AttackPlayer();
+    }
+
+    else if (playerInSightRange && !playerInAttackRange)
+    {
+      ChasePlayer();
+    }
+    else
+    {
+      Patroling();
+    }
+  }
+
+  void SearchWalkPoint()
+  {
+    //! Random värden
+    float randomZ = Random.Range(-walkPointRange, walkPointRange);
+    float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+    walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+    // !kollar om det är i marken
+    if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
+    {
+      walkpointSet = true;
+    }
+  }
+  void Patroling()
+  {
+    if (!walkpointSet)
+    {
+      SearchWalkPoint();
+    }
+
+    if (walkpointSet)
+    {
+      agent.SetDestination(walkPoint);
+    }
+
+    Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+    // !När Målet är nått
+    if (distanceToWalkPoint.magnitude < 1f)
+    {
+      walkpointSet = false;
+    }
+  }
+  void AttackPlayer()
+  {
+
+    // !Enemy rör ej
+    agent.SetDestination(transform.position);
+
+    // !Stalka player
+    transform.LookAt(player);
+
+    if (!alreadyAttacked)
+    {
+
+      // !Attack Koden:
+
+      //! 
+
+      alreadyAttacked = true;
+      Invoke(nameof(ResetAttacked), timeBetweenAttack);
+    }
+
+  }
+
+  void ResetAttacked()
+  {
+    alreadyAttacked = false;
+  }
+
+  void ChasePlayer()
+  {
+    // !Jagar spelaren
+    agent.SetDestination(player.position);
+  }
+
+  public void TakeDamage(float damageAmount)
+  {
+    // !Skada
+    health -= damageAmount;
+    if (health <= 0f)
+    {
+      Die();
+    }
+  }
+
+  void Die()
+  {
+    // !Dör
+    Destroy(gameObject);
+  }
+
+}
+
---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+
+  public float damage = 20f;
+  public float range = 100f;
+  public float impactForce = 100f;
+  public float fireRate = 20f;
+
+  public Camera aimCam;
+
+  public ParticleSystem flash;
+
+  public GameObject effect;
+
+  float fireTime = 0f;
+  void Update()
+  {
+    // !Kontroll
+    if (Input.GetButton("Fire1") && Time.time >= fireTime)
+    {
+      fireTime = Time.time + 1f / fireRate;
+      Shoot();
+    }
+  }
+  void Shoot()
+  {
+    // !Skottet
+    RaycastHit hit;
+    if (Physics.Raycast(aimCam.transform.position, aimCam.transform.forward, out hit, range))
+    {
+
+      // ?Fansy mansy Flashy helpy 
+      flash.Play();
+
+      EnemyAI target = hit.transform.GetComponent<EnemyAI>();
+
+      // !Skadar Target
+      if (target != null)
+      {
+        target.TakeDamage(damage);
+      }
+
+      // !Force
+      if (hit.rigidbody != null)
+      {
+        hit.rigidbody.AddForce(-hit.normal * impactForce);
+      }
+
+      // !Effekt
+      GameObject effektGO = Instantiate(effect, hit.point, Quaternion.LookRotation(hit.normal));
+      Destroy(effektGO, 1f);
+
+    }
+  }
+
+}
