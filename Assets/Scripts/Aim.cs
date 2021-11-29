4d7c46a52b473f8cdbcbb65aaebdb4c5036b7ebd
W3ndig0 <70271139+W3ndig0u0@users.noreply.github.com>
2021-11-29 21:59:51 +0100

    Yes

--- Assets/Scripts/Aim.cs
+++ Assets/Scripts/Aim.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Aim : MonoBehaviour
+public class PlayerAime : MonoBehaviour
 {
 
   public float mouseSensitivity = 101f;
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMove : MonoBehaviour
 {
   public CharacterController controller;
   public Transform groundCheck;
